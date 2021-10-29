801bf2ebbd50685c31438710d650837c8a4c5afb
Sanjeet Tiwari <sanjeettiwari00076@gmail.com>
2021-10-30 02:29:42 +0530

    build a very basic vanilla JS starter

---
+++ dist/index.017db2f2.js
@@ -0,0 +1,5 @@
+const a = 10;
+const b = 10;
+console.log("The sum is -", a + b);
+
+//# sourceMappingURL=index.017db2f2.js.map
---
+++ src/index.js
@@ -0,0 +1,3 @@
+const a = 10;
+const b = 10;
+console.log("The sum is -", a + b);
