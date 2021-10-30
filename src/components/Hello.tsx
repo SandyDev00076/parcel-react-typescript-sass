0a9c51c1ee3800e09c0a6f6856295da29de9ca04
Sanjeet Tiwari <sanjeettiwari00076@gmail.com>
2021-10-31 00:42:29 +0530

    remove scss files

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,9 +1,8 @@
 import React from "react";
-// import * as styles from "./Hello.module.scss";
-import "./Hello.scss";
+import * as styles from "./Hello.module.scss";
 
 const Hello = () => {
-  return <h1 className="name">Hello</h1>;
+  return <h1 className={styles.name}>Hello</h1>;
 };
 
 export default Hello;
